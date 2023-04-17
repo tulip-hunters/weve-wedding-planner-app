75a21cfa0558022453cc0ce642016c8df4f81079
bartoszde <bartas.de@gmail.com>
2023-04-17 18:58:33 +0200

    implement uploading images to cloudinary

--- routes/venue.routes.js
+++ routes/venue.routes.js
@@ -7,6 +7,23 @@ const Reservation = require("../models/Reservation.model");
 const attachCurrentUser = require("../middleware/attachCurrentUser");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
+const fileUploader = require("../config/cloudinary.config");
+
+// POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+ 
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  
+  res.json({ fileUrl: req.file.path });
+});
+
 //CREATE venue
 router.post("/venues", isAuthenticated, (req, res, next) => {
   const { name, description, address, price, capacity, imageUrl, offers } =
